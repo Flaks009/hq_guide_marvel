58e7a8f182a2175c7f13f17d6219d44dbde64715
Flaks009 <bruno.flaks@aluno.faculdadeimpacta.com.br>
2019-05-23 19:16:28 -0300

    Beta

--- dao/characters_insert.py
+++
@@ -1,9 +0,0 @@
-from infra.db import con
-from wrap_connection import transact
-
-sql_insert = "INSERT INTO Characters (name) VALUES (?)"
-
-@transact(con)
-def insertCharacters(characterName):
-    cursor.execute(sql_insert, (characterName, ))
-    cursor.commit()
